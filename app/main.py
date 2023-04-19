037bffd4da147e5324673961faefef5f69582d58
Sacha FARINEL <97121186+SachaFARINEL@users.noreply.github.com>
2023-04-19 18:27:38 +0200

    fix: path from app

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 from motor import motor_asyncio
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-from routers import users, tracks
+from .routers import users, tracks
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -10,7 +10,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(tracks.router)
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 load_dotenv()
 
 client = motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
