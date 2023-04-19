ed83c201b1636395c702f5934d73a1458a198a47
Sacha FARINEL <97121186+SachaFARINEL@users.noreply.github.com>
2023-04-19 17:37:38 +0200

    feat: jinga template

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from app.models.users import userModel
 
 router = APIRouter()
 #print(db)
@@ -6,4 +7,4 @@ router = APIRouter()
 
 @router.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": userModel}
